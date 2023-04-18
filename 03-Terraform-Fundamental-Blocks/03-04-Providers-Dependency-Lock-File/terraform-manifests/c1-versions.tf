85360a87bfaae4bcddc9db76e295c7e8283bad9e
svk1427 <75059407+svk1427@users.noreply.github.com>
2023-04-18 22:38:41 +0530

    Update c1-versions.tf

--- 03-Terraform-Fundamental-Blocks/03-04-Providers-Dependency-Lock-File/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Fundamental-Blocks/03-04-Providers-Dependency-Lock-File/terraform-manifests/c1-versions.tf
@@ -6,7 +6,7 @@ terraform {
     # AWS Provider 
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0.0"
+      version = ">= 2.0.0"  #this is our version constraint
     }
     # Random Provider
     random = {
@@ -21,3 +21,6 @@ provider "aws" {
   region = "us-east-1"
   profile = "default" # Defining it for default profile is Optional
 }
+
+#mana config ni future lo eadaina change cheyali antey manam latest version esthey present mana config previous version ki related ga vuntadi
+#suddenga manam new version echestey aws provider lo config lo diff errors ochestai ela rakunda vundadaniki e depend lock file use chestam
