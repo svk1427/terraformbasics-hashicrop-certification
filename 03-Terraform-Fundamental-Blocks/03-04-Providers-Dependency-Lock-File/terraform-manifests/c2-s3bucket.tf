90d0d3ea7daa4e98853c617088257ba47cc9af3b
svk1427 <75059407+svk1427@users.noreply.github.com>
2023-04-18 22:08:18 +0530

    Update c2-s3bucket.tf

--- 03-Terraform-Fundamental-Blocks/03-04-Providers-Dependency-Lock-File/terraform-manifests/c2-s3bucket.tf
+++ 03-Terraform-Fundamental-Blocks/03-04-Providers-Dependency-Lock-File/terraform-manifests/c2-s3bucket.tf
@@ -10,3 +10,11 @@ resource "aws_s3_bucket" "sample" {
   acl    = "public-read"
   region = "us-east-1"  # Comment this if we are going to use AWS Provider v3.x version
 }
+
+#this dependancy lock file primarly using for provider version locking
+# e DL endukantey version constraints vundadam valla, mana config ki ea version correct ani edhi deceide chesthundi
+#once edhi create ayyaka manam ah code repo lo peditey future lo appudu use chesina same version vuntundi
+#it curently works for provider dependnecies not for module
+#when you setup a version then it will download a specofic version constraint and check the errors in future with checksums
+#e lock file lekapotey manam oka verison ni default ga provide chestey adhi latest version thiseskuntadi ala kakunda -
+#manam lock file tho chestey specific version create avutadi mana confug ki thaggattuga
