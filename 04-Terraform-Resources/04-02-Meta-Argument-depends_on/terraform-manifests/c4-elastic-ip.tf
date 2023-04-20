b7a7392ac808a4ddcf31fc26e0056ba8340d4cce
svk1427 <75059407+svk1427@users.noreply.github.com>
2023-04-20 11:16:25 +0530

    Update c4-elastic-ip.tf

--- 04-Terraform-Resources/04-02-Meta-Argument-depends_on/terraform-manifests/c4-elastic-ip.tf
+++ 04-Terraform-Resources/04-02-Meta-Argument-depends_on/terraform-manifests/c4-elastic-ip.tf
@@ -5,3 +5,10 @@ resource "aws_eip" "my-eip" {
   # Meta-Argument
   depends_on = [aws_internet_gateway.vpc-dev-igw]
 }
+#EIP may require IGW to exist prior to association. Use depends_on to set an explicit dependency on the IGW.
+#here eip allocate avvali antey mundu igw vpc attach aie vundali adhi jarigina tarvata eip create avutadi.
+#ekkada resB antey eip and resA antey igw, resA create ayyaka matrame resB create avuthundi.
+
+#automatically provider dependencies are there and explicit dependecies are there
+#APD means genearally it comes along with terraform like in ec2 instance creation public ip will be assign once the ec2 created
+#but ED is manually dependson meta arg will deceide to create resB dependson resA like above 3 lines
