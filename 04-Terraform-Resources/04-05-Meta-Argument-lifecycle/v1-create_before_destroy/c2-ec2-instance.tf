04f3542f5b2e0a17bf63caa477b08b5df0ee2032
svk1427 <75059407+svk1427@users.noreply.github.com>
2023-04-21 11:30:13 +0530

    Update c2-ec2-instance.tf

--- 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v1-create_before_destroy/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v1-create_before_destroy/c2-ec2-instance.tf
@@ -14,3 +14,14 @@ resource "aws_instance" "web" {
   }
 
 }
+
+#this is also a behaviour changes of res meta arg
+#lifecycle is a nested block that can appear within a resource block
+#3 types of lifecycles are available 
+#1.Create_before_destroy - fir ex, us-east-1a lo config ni destroy chesi us-east-2b lo create cheyyali annappudu edhi use chestey,first edhi 2b lo infra mothham create
+#-chesina tarvata 1a lo infra ni destroy chestadi so thats why we called createbeforedestroy.
+#2.Prevent_destroy - for ex, eadaina imp config elanti situation lo kuda delete avvakudadhu annappudu e preventdestroy use chestam edhi use chestey ea usecase lo aiena 
+#-sare mana config delete/destroy avvadhu even though u applied destroy command kani ah specific res ni remove chesi apply chestey matram delete/destroy ipothundi.
+#3.Ignore changes - okasari infra create ipoyaka, eadaina mandatary scenario/usecase valla okokkasari manual ga aws console  nundi changes chestam, chesina tarvata
+#future lo eppudaina tf apply cmnd esthey manual changes anni delete ipothai so ala delete avvakunda ah particular manual changes ni ignore cheydaniki e LC use chestam.
+#eavaitey manam manual ga add chesamo ah particular res lo vatini comma sperated values lo echi eppudaina migatha infra ni execute chestey evi dellete avvakunda vuntai.
