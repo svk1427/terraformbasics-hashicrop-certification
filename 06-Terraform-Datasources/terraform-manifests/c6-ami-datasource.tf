91e5fabbbc004aa93e82cce32c680fb6f3f4cb68
svk1427 <75059407+svk1427@users.noreply.github.com>
2023-04-26 17:23:01 +0530

    Update c6-ami-datasource.tf

--- 06-Terraform-Datasources/terraform-manifests/c6-ami-datasource.tf
+++ 06-Terraform-Datasources/terraform-manifests/c6-ami-datasource.tf
@@ -1,6 +1,6 @@
 # Get latest AMI ID for Amazon Linux2 OS
-data "aws_ami" "amzlinux" {
-  most_recent = true
+data "aws_ami" "amzlinux" { #data means this a data resource and aws_ami means it is a data source we can call
+  most_recent = true        #oka dr ki oka ds vuntadi, adhi ea kind of argu ni read cheyyyalo chuskuntadi
   owners = [ "amazon" ]
   filter {
     name = "name"
@@ -19,3 +19,9 @@ data "aws_ami" "amzlinux" {
     values = [ "x86_64" ]
   }
 }
+
+#the whole code block is called data bloack
+#migataha vatiki lagane depends_on mtea.arg kuda e data.res ki support chestai
+#alagey mult.provider kuda dhiniki support chestadi
+#lifecycle support cheydhu
+#count and foreach support chestai
